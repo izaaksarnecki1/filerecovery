f3b968197ff5efe2f1f8d713c0042bfb8ba968bb
Izaak Sarnecki <izaaksarnecki@outlook.com>
2026-05-29 21:57:41 +1000

    store offsets of start of pics

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+off_t *array_add_elem(off_t *arr, int num_elems, int *capacity,
+                      off_t elem_to_add);
+
+void print_arr(off_t *arr, int num_elems);
 
 int main(int argc, char *argv[]) {
 
@@ -41,22 +47,52 @@ int main(int argc, char *argv[]) {
 
   unsigned char *regionvals = (unsigned char *)mappedregion;
 
+  int pics_capacity = 64;
   int num_pics = 0;
-  // off_t offsets[1000];
+  off_t *offsets = malloc(pics_capacity * sizeof(off_t));
+
+  if (offsets == NULL) {
+    fprintf(stderr, "Failed allocating.\n");
+    return 1;
+  }
 
   for (off_t i = 0; i < filestat.st_size - 3; i++) {
     if (regionvals[i] == 0xFF && regionvals[i + 1] == 0xD8 &&
         regionvals[i + 2] == 0xFF &&
         (regionvals[i + 3] == 0xE0 || regionvals[i + 3] == 0xE1)) {
+      offsets = array_add_elem(offsets, num_pics, &pics_capacity, i);
       num_pics++;
     }
   }
 
   printf("Num pics: %d\n", num_pics);
+  print_arr(offsets, num_pics);
 
+  free(offsets);
   munmap(mappedregion, filestat.st_size);
   fclose(fptr);
-
   printf("ALL GOOD!\n");
   return 0;
+}
+
+off_t *array_add_elem(off_t *arr, int num_elems, int *capacity,
+                      off_t elem_to_add) {
+  if (num_elems == *capacity - 1) {
+    *capacity *= 2;
+    off_t *tmp = realloc(arr, *capacity * sizeof(off_t));
+    if (tmp == NULL) {
+      fprintf(stderr, "Failed increasing size of array\n");
+      return arr;
+    }
+    arr = tmp;
+  }
+
+  arr[num_elems] = elem_to_add;
+  return arr;
+}
+
+void print_arr(off_t *arr, int num_elems) {
+  for (int i = 0; i < num_elems; i++) {
+    printf("offset %d: %lld\n", i, arr[i]);
+  }
 }
